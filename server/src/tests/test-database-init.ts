b55a4a82dcae44a15268ff957d0330139eaf8ac0
Patrice Pineault <TurboGraphxBeige@runbox.com>
2026-02-19 19:31:53 -0500

    test without data

--- server/src/tests/test-database-init.ts
+++ server/src/tests/test-database-init.ts
@@ -1,9 +1,19 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from 'sequelize-typescript';
 import fs from 'fs';
 import path from 'path'
 import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+
+import { Brand } from '../models/brand.js';
+import { Role } from '../models/role.js';
+import { Contact } from '../models/contact.js';
+import { Vehicle } from '../models/vehicle.js';
+import { VehiclePhoto } from '../models/vehicle_photo.js';
+import { VehicleModel } from '../models/model.js';
+import { Observation } from '../models/observation.js';
+import { User } from '../models/user.js';
+import { Service } from '../models/service.js';
 
 export const __filename = fileURLToPath(import.meta.url);
 export const __dirname = dirname(__filename);
@@ -16,11 +26,29 @@ if (!process.env.CI) {
 }
 
 // Initialize Sequelize
-const sequelize = new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PWD || '', {
+// const sequelize = new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PWD || '', {
+//     port: Number(process.env.DB_PORT) || 5432,
+//     host: process.env.DB_HOST || 'localhost',
+//
+//     dialect: 'postgres', // or 'postgres', 'sqlite', etc.
+// });
+const DB_HOST = process.env.DB_HOST ?? (() => { throw new Error('DB_HOST missing'); })();
+const DB_USER = process.env.DB_USER ?? (() => { throw new Error('DB_USER missing'); })();
+const DB_PWD = process.env.DB_PWD ?? (() => { throw new Error('DB_PASS missing'); })();
+const DB_NAME = process.env.DB_NAME ?? (() => { throw new Error('DB_NAME missing'); })();
+
+
+export const sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: DB_HOST,
+    username: DB_USER,
+    password: DB_PWD,
+    database: DB_NAME,
     port: Number(process.env.DB_PORT) || 5432,
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'postgres', // or 'postgres', 'sqlite', etc.
+    models: [Role, Brand, Contact, Vehicle, VehiclePhoto, VehicleModel, Observation, Service, User],
+    logging: false
 });
+
 
 // Sync function
 async function syncDatabase() {
