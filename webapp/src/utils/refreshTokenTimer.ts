630681e11547e65c6813707e47acecca6f3e50a4
Patrice Pineault <TurboGraphxBeige@runbox.com>
2026-03-21 23:17:50 -0400

    Refresh tokens

---
+++ webapp/src/utils/refreshTokenTimer.ts
@@ -0,0 +1,9 @@
+import apiService from '../services/api.service.ts'
+
+export default function startRefreshTimer() {
+    const refreshInterval = 1 * 60 * 1000; // 14 minutes (access token expiry time - 1 minute buffer)
+    const timer = setTimeout(() => {
+        apiService.refreshToken();
+    }, refreshInterval);
+    return timer;
+};
